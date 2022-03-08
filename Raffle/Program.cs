140dfdcea90e89e73fed9a38f2565ff2c9216235
viniciusyashuda <viniciusyashuda008@gmail.com>
2022-03-08 09:27:45 -0300

    Add project files.

---
+++ Raffle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Raffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int aux;
+            int[] vetor = new int[10];
+
+            Console.WriteLine("\nOrdered values:");
+
+            for (int k = 0; k < vetor.Length; k++)
+            {
+
+                vetor[0] = new Random().Next(0, 100);
+
+                for (int i = 0; i < vetor.Length; i++)
+                {
+
+                    for (int j = i + 1; j < vetor.Length; j++)
+                    {
+
+                        if (vetor[i] > vetor[j])
+                        {
+
+                            aux = vetor[i];
+                            vetor[i] = vetor[j];
+                            vetor[j] = aux;
+
+                        }
+
+                    }
+
+                }
+            }
+
+            for (int k = 0; k < vetor.Length; k++)
+            {
+
+                Console.WriteLine(vetor[k]);
+
+            }
+        }
+    }
+}
+
