c7862c9a25b5e2887e7b698905e1e5ea4ad71448
shuda <viniciusyashuda008@gmail.com>
2022-11-25 22:52:06 -0300

    melhorando a estrutura e implementando uma nova forma de fazer

--- Raffle/Program.cs
+++ Raffle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Raffle
 {
@@ -6,43 +7,36 @@ namespace Raffle
     {
         static void Main(string[] args)
         {
-
             int aux;
             int[] vetor = new int[10];
 
-            Console.WriteLine("\nOrdered values:");
+            Console.WriteLine("Ordered values:");
 
             for (int k = 0; k < vetor.Length; k++)
             {
-
                 vetor[0] = new Random().Next(0, 100);
 
                 for (int i = 0; i < vetor.Length; i++)
-                {
-
                     for (int j = i + 1; j < vetor.Length; j++)
-                    {
-
                         if (vetor[i] > vetor[j])
                         {
-
                             aux = vetor[i];
                             vetor[i] = vetor[j];
                             vetor[j] = aux;
-
                         }
-
-                    }
-
-                }
             }
+
+            //Using Linq:
+            var orderedUsingLinq = vetor.OrderBy(vetor => vetor);
+
+            foreach (var vetorLinq in orderedUsingLinq)
+                Console.WriteLine(vetorLinq);
+
+            Console.WriteLine("\n\n");
 
             for (int k = 0; k < vetor.Length; k++)
-            {
-
                 Console.WriteLine(vetor[k]);
 
-            }
         }
     }
 }
